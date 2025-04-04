6f8597d360519fa806f964a583077cf2bb218f4d
CUJeong <akow283@gmail.com>
2025-04-04 11:40:36 +0900

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 // index.html 내 div 태그를 선택해서 내부에 App 을 그리도록 적용
 createRoot(document.getElementById('root')).render(
+  // StrictMode 는 배포할때 영향을 주지 않으며, 개발 단계에서 문제점을 찾아줌 (실행을 두번씩 해서 조금 거슬림)
   <StrictMode>
     <App />
   </StrictMode>,
--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./Post.css";
 
@@ -5,16 +6,47 @@ function Post() {
     // localStorage 를 활용한 CRUD 페이지
     const navigate = useNavigate();
 
+    // return 이 되어야 랜더링이 시작되는데, 이와같은 코드형태는 데이터를 다 불러올때까지 랜더링이 되지 않음
+    /*
+
     // localStorage.getItem("postData") 가 null 인 경우에는 postData에 빈 배열 [] 을 담음
     let postData = JSON.parse(localStorage.getItem("postData")) || [];
-
+    
     // 최신순으로 정렬
     postData.sort((a, b) => {
         return b.no - a.no;
     });
 
+    */
+
+    // 데이터 항목을 랜더링 하기 전에 다른 항목들은 미리 랜더링해놓고,
+    // 이후 데이터를 불러와서 데이터 항목을 랜더링하는것이 사용자 입장에서 더 로딩이 빠르게 느껴짐
+    // 즉, return 이 실행되어 랜더링이 끝난 후 데이터를 불러오는 코드를 작성하기 위해 useEffect를 사용함
+    const [postData, setPostData] = useState([]);
+    const [isAsc, setAsc] = useState(true);
+
+    // 내부 함수는 return에 의한 랜더링이 끝난 후 실행됨
+    useEffect(() => {
+        console.log("useEffect 실행");
+        const data = JSON.parse(localStorage.getItem("postData")) || [];
+        if(isAsc){
+            data.sort((a, b) => a.no - b.no);
+        }else{
+            data.sort((a, b) => b.no - a.no);
+        }
+        setPostData(data);
+    }, [isAsc]);  // 두번째 파라미터에 빈 배열(의존성배열)을 넣으면 현재 페이지(Post.jsx) 진입 시 한번 실행되고 끝
+
+    // 두번째 파라미터의 배열 내에 변수값(state)을 넣어놓으면, 변수값 변동시 useEffect의 함수가 실행됨
+
+
     return (
         <div className="post-container">
+            <div className="sort-box">
+                <a onClick={()=>{
+                    setAsc(!isAsc);
+                }}>▼ 최신순</a>
+            </div>
             <table className="post-list">
                 <thead>
                     <tr>
@@ -29,6 +61,7 @@ function Post() {
                         postData.map((post) => (
                             // 이전에는 key에 index 를 넣었지만 비추방식이므로, 게시글의 PK값을 key 속성에 반영
                             <tr key={post.no}>
+                                {console.log("랜더링 중")}
                                 <td>{post.no}</td>
                                 <td>
                                     <Link
@@ -59,6 +92,7 @@ function Post() {
                     글쓰기
                 </button>
             </div>
+            {console.log("랜더링 끝")}
         </div>
     );
 }
